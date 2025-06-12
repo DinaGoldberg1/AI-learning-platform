e2e5ebd868cd1b3be3af2b2c00f18cf5542a6e6a
DinaGoldberg1 <Hand3D7@mbjcomp.org.il>
2025-06-13 01:18:05 +0300

    Backend part finished

--- SERVER/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ SERVER/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8213c6aa3ed66efc91554fc959af54810cbd3711")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a1c2d5d473bd8d80cf8483557a66ca9988b131")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
