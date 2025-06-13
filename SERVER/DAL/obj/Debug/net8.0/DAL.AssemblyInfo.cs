529cd29f0133f157596ce3bea48e0c415bcdfeaa
DinaGoldberg1 <Hand3D7@mbjcomp.org.il>
2025-06-13 07:13:46 +0300

    returning to last commit

--- SERVER/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ SERVER/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d734f41e450694fb4313758b48fa0b8b53fd30e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9db071ea60b80b63bce5856b6f0678014bee3fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
